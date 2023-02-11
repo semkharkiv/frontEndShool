08a52bbd113a7603c9a4c6d152f51820f05e99ad
abdullayevsema <info.pelikantobacco@gmail.com>
2023-02-11 15:12:02 +0100

    abc

---
+++ lesson9/scripts/script.js
@@ -0,0 +1,50 @@
+// console.log("Hello world!")
+
+// let name = prompt("Write your name");
+// console.log("Hello " + name);  //concontenation 
+// console.log(`Hello ${name}`);
+
+// console.log(5);
+// console.log("5");
+
+// console.log(typeof(name));
+// console.log(typeof(5));
+// console.log(typeof name);
+
+// const r = 12;
+// const g = 34;
+// const b = 14;
+// =======rgb(12,34,14)========
+// console.log(`interpolation rgb= (${r},${g},${b})`);
+
+// const value_1=12;
+// const value_2=4;
+
+// console.log(value_1 + value_2);
+// console.log(value_1 - value_2);
+// console.log(value_1 * value_2);
+// console.log(value_1 / value_2);
+// console.log(value_1 % value_2);
+// console.log(value_1 ** value_2);  //возведение в степень 
+
+// console.log("==============");
+// const a = 2;
+// const b = "2";
+// console.log(typeof String(a));
+// console.log(typeof Number(b));
+// console.log(typeof +b);
+// console.log(typeof("" + a));  
+
+// console.log("===================");
+// console.log(Number("a"));
+
+//=============================
+// let age = prompt("Write your age");
+// console.log(age*525600);
+
+//====================
+
+// let numb = prompt("Write a number")
+// console.log(numb**2);
+
+//=====================
